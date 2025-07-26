ecf83b0fd46b77e0139a69de8cb263671a19ff91
Vikas <vikkypermude@gmail.com>
2025-07-26 13:13:19 +0530

    adding the edited

--- Sorting/WaveSorting.java
+++ Sorting/WaveSorting.java
@@ -1,5 +1,23 @@
 package Sorting;
-
+import java.util.*;
 public class WaveSorting {
-    
+    public static void main(String[] args) {
+            int arr[]={10,90,49,2,1,5,23};
+            waveSort(arr);
+            for(int i:arr){
+                System.out.print(i+" ");
+            }
+    }
+    public static int[] waveSort(int[] arr){
+        Arrays.sort(arr);
+        int left=0;
+        int right=0;
+        for(int i=0;i<arr.length-1;i+=2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+
+        return arr;
+    }
 }
