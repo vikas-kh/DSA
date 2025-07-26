daeed2570c2f5c192489f64546416aa6939ab5eb
Vikas <vikkypermude@gmail.com>
2025-07-26 12:38:53 +0530

    add the window.java

---
+++ Srings/Window.java
@@ -0,0 +1,37 @@
+package Srings;
+
+import java.util.HashSet;
+import java.util.*;
+public class Window {
+    public static void main(String[] args) {
+        String str="pwwkew";
+        System.out.println(subString(str));
+    }
+    public static int subString(String str){
+        int p1=0;
+        int p2=0;
+        int max=0;
+        int n=str.length();
+        Set<Character> set=new HashSet<>();
+        
+        int count=0;
+        while(p1<n-1){
+            
+        while(p2<=n){
+            
+            if(set.contains(str.charAt(p2))){
+                set.removeAll(set);
+                max=Math.max(count,max);
+                
+                p1=p2+1;
+                break;
+            }
+            set.add(str.charAt(p2));
+            p2++;
+            count++;
+        }
+        
+    }
+    return count;
+    }
+}
