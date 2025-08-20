056a43e185fd597aa6a14c8d9a57905c3acdfd72
Vikas <vikkypermude@gmail.com>
2025-08-20 14:11:17 +0530

    leetcode 1752

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -22,10 +22,49 @@ class LinkedList{
     }
 
     public void addLast(String data){
-        
+        Node new_node=new Node(data);
+        if(head == null){
+            head=new_node;
+            return;
+        }
+        Node currNode=head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        } 
+        currNode.next=new_node;
     }
-    void display(){
-        
+    public void display(){
+        if(head==null){
+            System.out.println("empty");
+            return;
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.data + "->");
+            currNode=currNode.next;
+        } 
+        System.out.print("null");
+    }
+    //delete fisrt,last
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("empty");
+            return;
+        }
+        //only one element
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        //has many nodes
+        head=head.next;
+    }
+
+    puvlic void public void deleteLast(){
+        if(head==null){
+            System.out.println("empty");
+            return;
+        }
     }
 
 
@@ -33,5 +72,9 @@ class LinkedList{
         LinkedList list = new LinkedList();
         list.addFirst("a");
         list.addFirst("is");
+        list.addLast("vikas");
+        list.deleteFirst();
+         list.deleteFirst();
+        list.display();
     }
 }
---
+++ leetcode/Leetcode1752.java
@@ -0,0 +1,20 @@
+package leetcode;
+//1752. Check if Array Is Sorted and Rotated
+class Leetcode1752{
+    static public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }        
+        
+        return count <= 1;
+
+    }
+    public static void main(String[] args){ 
+        int[] arr={2,1};
+        System.out.println(check(arr));
+    }
+}
